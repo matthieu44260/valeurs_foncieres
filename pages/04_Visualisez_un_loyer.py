98cfed5465f92e167f213282a46b3c0352f57243
Matthieu El Hakim <matthieuelhakim@yahoo.fr>
2024-01-18 11:39:11 +0100

    ajout de la page des loyers

--- pages/04_Visualisez_un_loyer.py
+++ pages/04_Visualisez_un_loyer.py
@@ -1,21 +1,67 @@
 import streamlit as st
 import duckdb
 
-st.title("Recherchez le loyer d'un bien")
+st.title("Estimez le loyer d'un bien")
+st.markdown("Estimations ANIL, à partir des données du Groupe SeLoger et de leboncoin")
 st.divider()
 
-type_bien = st.selectbox(
-        'Choisissez le type de bien',
-        ['Maison', 'Appartement'],
-        index=None,
-        placeholder='Type de bien')
+calcul_loyer = None
+commune = None
+
+col_a, col_b, col_c = st.columns(3)
+with col_a:
+    type_bien = st.selectbox(
+            'Choisissez le type de bien',
+            ['Maison', 'Appartement'],
+            index=None,
+            placeholder='Type de bien'
+    )
 
 if type_bien:
     if type_bien == 'Maison':
         con = duckdb.connect(database="donnees_immo/loyer_maison.duckdb", read_only=False)
     if type_bien == 'Appartement':
         con = duckdb.connect(database="donnees_immo/loyer_appt.duckdb", read_only=False)
+    department_choice = con.execute("SELECT DISTINCT DEP FROM table_donnees ORDER BY DEP").df()
+    with col_b:
+        departement = st.selectbox(
+            'Choisissez votre département',
+            department_choice,
+            index=None,
+            placeholder='Département'
+        )
 
-    query = ("SELECT * FROM table_donnees")
-    df = con.execute(query).df()
-    st.dataframe(df)
+    if departement:
+        city_choice = con.execute(f"SELECT DISTINCT commune FROM table_donnees WHERE DEP = '{departement}' "
+                                  f"ORDER BY commune").df()
+        with col_c:
+            commune = st.selectbox(
+                'Choisissez votre commune',
+                city_choice,
+                index=None,
+                placeholder='Commune'
+            )
+
+if commune:
+    df = con.execute(f"SELECT * FROM table_donnees WHERE DEP = '{departement}' AND commune = '{commune}'").df()
+    fourchette_basse = con.execute(f"SELECT fourchette_basse_au_m2 FROM table_donnees WHERE DEP = '{departement}' "
+                                   f"AND commune = '{commune}'").fetchone()[0]
+    fourchette_haute = con.execute(f"SELECT fourchette_haute_au_m2 FROM table_donnees WHERE DEP = '{departement}' "
+                                   f"AND commune = '{commune}'").fetchone()[0]
+    st.markdown(" ")
+    st.markdown(f"Les loyers pour un bien ({type_bien}) dans la commune de {commune} varient entre "
+                f":blue[{fourchette_basse}] et :blue[{fourchette_haute}] € au m².")
+    st.markdown(" ")
+    col_d, col_e, col_f = st.columns(3)
+    with col_d:
+        calcul_loyer = st.number_input(
+            "Indiquez une surface en m²",
+            min_value=0,
+            value=None
+        )
+
+if calcul_loyer:
+    loyer_bas = int(fourchette_basse*calcul_loyer)
+    loyer_haut = int(fourchette_haute*calcul_loyer)
+    st.markdown(f"Pour une surface de {calcul_loyer} m², "
+                f"les loyers varient entre :blue[{loyer_bas}] et :blue[{loyer_haut}] €.")
